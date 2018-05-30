7bf0f9752a1e4ae38e448ec39112c2153b33b52c
Jeff <jeff@JeffdeMBP.Home>
2018-05-30 23:48:31 +0800

    add more console.log

--- 3.1/async-callback.js
+++ 3.1/async-callback.js
@@ -5,17 +5,23 @@ const readFileAsArray = function(file, cb) {
     if (err) {
       return cb(err);
     }
-
+    
     const lines = data.toString().trim().split('\n');
+    console.log('3');
     cb(null, lines);
+    console.log('5');
   });
 };
 
+console.log('1');
 // example call
 readFileAsArray('./numbers', (err, lines) => {
   if (err) throw err;
 
+  console.log('4');
+  
   const numbers = lines.map(Number);
   const oddNumbers = numbers.filter(number => number % 2 === 1);
   console.log('odd numbers count:', oddNumbers.length);
 });
+console.log('2');
